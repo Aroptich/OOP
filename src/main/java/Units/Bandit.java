34528a967a34cb259fbf7e031553ce886a4ba6b2
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-02-21 08:18:50 +0700

    Update Bandit.java
    Edit Bandit

--- src/main/java/Units/Bandit.java
+++ src/main/java/Units/Bandit.java
@@ -1,14 +1,33 @@
-package Units;
+package Unit;
 
-public class Bandit extends Character{
+public class Bandit extends Infantry{
 
+    int disguise;
 
-    public Bandit(int attack, int armor, int damage, int health, int speed, String name) {
-        super(attack, armor, damage, health, speed, name="Разбойник");
+    public Bandit(String name, Float hp, Integer maxHp, Integer attack, Integer minDamage, Integer maxDamage, Integer protection, int disguise) {
+        super(name, hp, maxHp, attack, minDamage, maxDamage, protection);
+        this.disguise = disguise;
     }
 
-    public int getTheft(){
-        //Воровство: Ворует снаряды у стрелков
-        return 5;
+    public Bandit(String name) {
+        super(name, 15.1f, 15, 7, 5, 3, 6);
+        this.disguise = 15;
+    }
+
+    public void steal(Shooter Shooter){
+    }
+
+    @Override
+    public StringBuilder getInfo() {
+        StringBuilder builder = new StringBuilder();
+        return builder.append("Бандит: \t").append(Bandit.super.name)
+                .append("\t ATK: \t").append(Bandit.super.attack)
+                .append("\t HP: \t").append(Bandit.super.hp)
+                .append("\t Speed: \t").append(Bandit.super.speed);
+    }
+
+    @Override
+    public void step() {
+
     }
 }
