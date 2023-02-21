d2af1fa77f364f194991b34a9cbc1c4cf07447ef
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-02-21 08:30:07 +0700

    Create Infantry.java
    Add Infantry

---
+++ src/main/java/Units/Infantry.java
@@ -0,0 +1,15 @@
+package Units;
+
+public abstract class Infantry extends Human{
+
+    public Infantry(String name, Float hp, Integer maxHp, Integer attack, Integer minDamage, Integer maxDamage,
+                    Integer protection) {
+        super(name, hp, maxHp, attack, minDamage, maxDamage, protection, 3);
+    }
+
+
+    @Override
+    public Integer getAttack() {
+        return attack;
+    }
+}
