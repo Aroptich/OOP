78801cccaca694333e351bea9ba7e90fd0396c51
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-02-21 08:28:37 +0700

    Update Spearman.java
    Edit Spearman

--- src/main/java/Units/Spearman.java
+++ src/main/java/Units/Spearman.java
@@ -1,11 +1,25 @@
 package Units;
 
-public class Spearman extends Character{
-    public Spearman(int attack, int armor, int damage, int health, int speed, String name) {
-        super(attack, armor, damage, health, speed, name="Копейщик");
+public class Spearman extends Infantry{
+    public Spearman(String name, Float hp, Integer maxHp, Integer attack, Integer minDamage, Integer maxDamage, Integer protection) {
+        super(name, hp, maxHp, attack, minDamage, maxDamage, protection);
     }
-    public int getPiercing_Thrust(){
-        //Пронзительный кол - игнорирует 30% защиты противника
-        return 15;
+
+    public Spearman(String name) { super(name, 20.1f,20,10, 9,5, 7); }
+
+    public void runAway() {    }
+
+    @Override
+    public StringBuilder getInfo() {
+        StringBuilder builder = new StringBuilder();
+        return builder.append("Копейщик: \t").append(Spearman.super.name)
+                .append("\t ATK: \t").append(Spearman.super.attack)
+                .append("\t HP: \t").append(Spearman.super.hp)
+                .append("\t Speed: \t").append(Spearman.super.speed);
+    }
+
+    @Override
+    public void step() {
+
     }
 }
