0fded85f7117221ca966a2cf6ea60513b06cb3a6
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-02-21 08:25:37 +0700

    Create Names.java
    Add Names

---
+++ src/main/java/Units/Names.java
@@ -0,0 +1,15 @@
+package Unit;
+
+public enum Names {
+        Nata,
+        Riki,
+        Mike,
+        Kate,
+        Dima,
+        Vika,
+        Oleg
+        ;
+
+        private Names() {
+        }
+}
