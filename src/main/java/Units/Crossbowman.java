b37769ebd263054d3f857ca6bcd80ec3fc11ea58
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-02-21 08:19:27 +0700

    Update Crossbowman.java
    Edit Crossbowman

--- src/main/java/Units/Crossbowman.java
+++ src/main/java/Units/Crossbowman.java
@@ -1,15 +1,29 @@
-package Units;
+package Unit;
 
-public class Crossbowman extends Character{
-    int shells; // Показатель снарядов
+public class Crossbowman extends Shooter{
 
-    public Crossbowman(int attack, int armor, int damage, int health, int speed, String name, int shells) {
-        super(attack, armor, damage, health, speed, name);
-        this.shells = shells;
+    public Crossbowman(String name) {
+        super(name, 150.1f, 150, 10, 10, 5, 3,
+                5,7,6);
     }
 
-    public int getHeadshot(){
-        //Выстрел в голову - игнорирует защиту противника
-        return 25;
+
+    public Crossbowman(String name, Float hp, Integer maxHp, Integer attack, Integer minDamage, Integer maxDamage,
+                       Integer protection, Integer speed, int cartridges, int range) {
+        super(name, hp, maxHp, attack, minDamage, maxDamage, protection, speed, cartridges, range);
     }
+
+    @Override
+    public Integer getAttack() { return attack; }
+
+    @Override
+    public StringBuilder getInfo() {
+        StringBuilder builder = new StringBuilder();
+        return builder.append("Арбалет: \t").append(Crossbowman.super.name)
+                .append("\t ATK: \t").append(Crossbowman.super.attack)
+                .append("\t HP: \t").append(Crossbowman.super.hp)
+                .append("\t Arrows: ").append(Crossbowman.super.cartridges)
+                .append("\t Speed: \t").append(Crossbowman.super.speed);
+    }
+
 }
