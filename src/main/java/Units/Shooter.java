da20f926cbd59161453e3759626af2a82706d734
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-02-21 08:26:19 +0700

    Create Shooter.java
    Add Shooter

---
+++ src/main/java/Units/Shooter.java
@@ -0,0 +1,29 @@
+package Units;
+
+public abstract class Shooter extends Human{
+    int cartridges;
+    int range;
+
+    public Shooter(String name, Float hp, Integer maxHp, Integer attack, Integer minDamage, Integer maxDamage,
+                   Integer protection, Integer speed, int cartridges, int range) {
+        super(name, hp, maxHp, attack, minDamage, maxDamage, protection, speed);
+        this.cartridges = cartridges;
+        this.range = range;
+    }
+
+    @Override
+    public void step() {
+        int cart = getCartridges();
+        if (cart > 0) {
+            setCartridges(cart-1);
+        }
+    }
+
+    public int getCartridges() {
+        return this.cartridges;
+    }
+
+    public void setCartridges(int cartridges) {
+        this.cartridges = cartridges;
+    }
+}
