14967d7913b366bbf3af14ea81428e51b7aa025d
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-02-24 11:40:03 +0700

    Update Farmer.java

--- src/main/java/Units/Farmer.java
+++ src/main/java/Units/Farmer.java
@@ -1,29 +1,31 @@
-package Units;
+package OOP.Units;
 
-public class Farmer extends Human{
-    int delivery;
+public class Farmer extends Human {
+    protected static int cartridges;
+    public Farmer(String name, float hp, int maxHp, int attack, int damageMin, int damageMax, int defense,
+                  int speed, int cartridges, int posX, int posY) {
+        super(name, hp, maxHp, attack, damageMin, damageMax, defense, speed);
+        this.cartridges = cartridges;
+    }
     public Farmer(String name) {
-        super(name, 10.1f, 10, 3,1,3,1, 1);
-        this.delivery = 1;
+        super(name, 150.f, 1, 1, 1, 1, 1,
+                1);
+        this.cartridges = 1;
     }
 
-    public Farmer(String name, Float hp, Integer maxHp, Integer attack, Integer minDamage, Integer maxDamage, Integer protection, Integer speed) {
-        super(name, hp, maxHp, attack, minDamage, maxDamage, protection, speed);
+    public void armiger(Shooter Shooter){  }
+
+    static int getCartridges() {
+        return cartridges;
     }
-
-    @Override
-    public void step() {
-
+    void setCartridges(int o) {
+        this.cartridges = cartridges - o;
     }
-
-
     @Override
     public StringBuilder getInfo() {
         StringBuilder builder = new StringBuilder();
         return builder.append("Фермер: \t").append(Farmer.super.name)
-                .append("\t ATK: \t").append(Farmer.super.attack)
-                .append("\t HP: \t").append(Farmer.super.hp);
-    }
-
-
+                .append("\t| ATK:\t").append(Farmer.super.attack)
+                .append("\t| HP:\t").append(Farmer.super.hp)
+               }
 }
