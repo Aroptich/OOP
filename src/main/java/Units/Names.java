b93608b00e65941154979d5c8c69aadabc03e6b6
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-02-24 11:42:52 +0700

    Update Names.java

--- src/main/java/Units/Names.java
+++ src/main/java/Units/Names.java
@@ -1,15 +1,7 @@
 package Units;
 
 public enum Names {
-        Nata,
-        Riki,
-        Mike,
-        Kate,
-        Dima,
-        Vika,
-        Oleg
-        ;
-
-        private Names() {
-        }
+    Natalie, Rick, Ivan, Katya, Dima, Volodya, Fedya, John, Zhenya, Olya, Alexei, Peter, Arthur, Misha, Masha,
+    Grisha, Maxim, Alex, Xenia, Elisha, Dobi, Evgenia, Artem, Mitya, Vitya, Hope, Love, Faith, Xenya,
+    Achil, Kolya, Nikolai, Arcania, Sofia, Christi, Alena, Alina
 }
