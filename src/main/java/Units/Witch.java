afcaeac7dd81b792a4c03408fdb121233d283801
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-02-24 11:45:46 +0700

    Update Witch.java

--- src/main/java/Units/Witch.java
+++ src/main/java/Units/Witch.java
@@ -1,33 +1,27 @@
-ackage Units;
+package Units;
 
-public class Witch extends Magic {
+public class Witch extends Mag{
 
-
-    public Witch(String name, Float hp, Integer maxHp, Integer attack, Integer minDamage, Integer maxDamage,
-                 Integer protection, int mana, int maxMana) {
-        super(name, hp, maxHp, attack, minDamage, maxDamage, protection, mana, maxMana);
+    public Witch(String name, float hp, int maxHp, int attack, int damageMin, int damageMax,
+                 int defense, int speed, int mana, int maxMana, int posX, int posY ) {
+        super(name, hp, maxHp, attack, damageMin, damageMax, defense, speed, mana, maxMana);
     }
-
     public Witch(String name) {
-        super(name, 15.1f, 15, 10, 8, 7, 2, 15, 15);
+        super(name, 130.f, 130, 15, 15, 5, 2,
+                6, 6 ,5);
     }
 
     @Override
-    public Integer getAttack() {
+    public int getAttack() {
         return attack;
-    }
-
-    @Override
-    public void step() {
-
     }
 
     @Override
     public StringBuilder getInfo() {
         StringBuilder builder = new StringBuilder();
         return builder.append("Колдун: \t").append(Witch.super.name)
-                .append("\t ATK: \t").append(Witch.super.attack)
-                .append("\t HP: \t").append(Witch.super.hp)
-                .append("\t MP: \t").append(Witch.super.mana);
-    }
+                .append("\t| ATK:\t").append(Witch.super.attack)
+                .append("\t| HP:\t").append(Witch.super.hp)
+                .append("\t| MP:\t").append(Witch.super.mana)
+                }
 }
