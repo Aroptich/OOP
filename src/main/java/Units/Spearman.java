3efc3563bb6a05f357d7205a41e81c62a315c811
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-02-24 11:44:17 +0700

    Update Spearman.java

--- src/main/java/Units/Spearman.java
+++ src/main/java/Units/Spearman.java
@@ -1,25 +1,24 @@
 package Units;
 
-public class Spearman extends Infantry{
-    public Spearman(String name, Float hp, Integer maxHp, Integer attack, Integer minDamage, Integer maxDamage, Integer protection) {
-        super(name, hp, maxHp, attack, minDamage, maxDamage, protection);
+public class Spearman extends Warrior {
+    public Spearman(String name, float hp, int maxHp, int attack, int damageMin, int damageMax, int defense, int speed, int posX, int posY) {
+        super(name, hp, maxHp, attack, damageMin, damageMax, defense, speed);
     }
 
-    public Spearman(String name) { super(name, 20.1f,20,10, 9,5, 7); }
+    public Spearman(String name) {
+
+        super(name, 200.f, 200, 30, 5, 6, 5,
+                4);
+    }
 
     public void runAway() {    }
 
     @Override
     public StringBuilder getInfo() {
         StringBuilder builder = new StringBuilder();
-        return builder.append("Копейщик: \t").append(Spearman.super.name)
-                .append("\t ATK: \t").append(Spearman.super.attack)
-                .append("\t HP: \t").append(Spearman.super.hp)
-                .append("\t Speed: \t").append(Spearman.super.speed);
-    }
-
-    @Override
-    public void step() {
-
-    }
+        return builder.append("Копейщик:\t").append(Spearman.super.name)
+                .append("\t| ATK:\t").append(Spearman.super.attack)
+                .append("\t| HP:\t").append(Spearman.super.hp)
+                }
 }
+
