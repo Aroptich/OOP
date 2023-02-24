0cfc2a7ea2dda599cf730d40f1fee8be00f68deb
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-02-24 11:44:42 +0700

    Update Sniper.java

--- src/main/java/Units/Sniper.java
+++ src/main/java/Units/Sniper.java
@@ -3,26 +3,21 @@ package Units;
 public class Sniper extends Shooter{
 
     public Sniper(String name) {
-        super(name, 150.1f, 150, 10, 10, 5, 3, 4,
-                10, 5);
-    }
-
-    public Sniper(String name, Float hp, Integer maxHp, Integer attack, Integer minDamage, Integer maxDamage,
-                  Integer protection, Integer speed, int cartridges, int range) {
-        super(name, hp, maxHp, attack, minDamage, maxDamage, protection, speed, cartridges, range);
+        super(name, 110.f, 110, 8, 6, 8, 6,
+                8, 6, 5);
     }
 
     @Override
-    public Integer getAttack() {
+    public int getAttack() {
         return attack;
     }
 
     @Override
     public StringBuilder getInfo() {
         StringBuilder builder = new StringBuilder();
-        return builder.append("Снайпер: \t").append(Sniper.super.name)
-                .append("\t ATK: \t").append(Sniper.super.attack)
-                .append("\t HP: \t").append(Sniper.super.hp)
-                .append("\t Arrows: ").append(Sniper.super.cartridges);
-    }
+        return builder.append("Снайпер:\t").append(Sniper.super.name)
+                .append("\t| ATK:\t").append(Sniper.super.attack)
+                .append("\t| HP:\t").append(Sniper.super.hp)
+                .append("\t| Arrows: ").append(Sniper.super.cartridges)
+               }
 }
