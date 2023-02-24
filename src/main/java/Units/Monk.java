2364a8d4f7fb8bc8264bf155ebd53b31a4172e54
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-02-24 11:42:30 +0700

    Update Monk.java

--- src/main/java/Units/Monk.java
+++ src/main/java/Units/Monk.java
@@ -1,36 +1,28 @@
 package Units;
 
-public class Monk extends Magic {
+public class Monk extends Mag{
 
     int FreezeStep;
-
-    public Monk(String name, Float hp, Integer maxHp, Integer attack, Integer minDamage, Integer maxDamage,
-                Integer protection, int mana, int maxMana, int freezeStep) {
-        super(name, hp, maxHp, attack, minDamage, maxDamage, protection, mana, maxMana);
-        FreezeStep = freezeStep;
+    public Monk(String name, float hp, int maxHp, int attack, int damageMin, int damageMax,
+                int defense, int speed, int mana, int maxMana) {
+        super(name, hp, maxHp, attack, damageMin, damageMax, defense, speed, mana, maxMana);
+        this.FreezeStep = FreezeStep;
     }
-
     public Monk(String name) {
-        super(name,10.1f,10,8,8,6,2,10, 10);
+        super(name, 130.f, 130, 15, 15, 5, 2,
+                60, 6 ,5);
         this.FreezeStep = 7;
     }
-
-    public Integer FreezeWitch(Witch witch){
+    public int FreezeWitch(Witch witch){
         return FreezeStep;
     }
 
     @Override
     public StringBuilder getInfo() {
         StringBuilder builder = new StringBuilder();
-        return builder.append("Монах: \t").append(Monk.super.name)
-                .append("\t ATK: \t").append(Monk.super.attack)
-                .append("\t HP: \t").append(Monk.super.hp)
-                .append("\t MP: \t").append(Monk.super.mana);
-
-    }
-
-    @Override
-    public void step() {
-
-    }
+        return builder.append("Монах:  \t").append(Monk.super.name)
+                .append("\t| ATK:\t").append(Monk.super.attack)
+                .append("\t| HP:\t").append(Monk.super.hp)
+                .append("\t| MP:\t").append(Monk.super.mana)
+                }
 }
