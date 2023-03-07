29869d22f8eafdbabbe22a88e279f6eb77334f11
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-03-07 13:02:58 +0700

    Update Shooter.java

--- src/main/java/Units/Shooter.java
+++ src/main/java/Units/Shooter.java
@@ -1,75 +1,66 @@
 package Units;
 
+import java.util.ArrayList;
+
 public abstract class Shooter extends Human {
     protected int range;
-    protected int cartridges;
+    protected int arrows;
 
-    protected Shooter(String name, float hp, int maxHp, int attack, int damageMin,
-                   int damageMax, int defense, int speed, int cartridges,
-                   int range, int posX, int posY) {
+    protected Shooter(String name, float hp, int maxHp, int attack, int damageMin, int damageMax,
+                      int defense, int speed, int arrows, int range, int posX, int posY) {
         super(name, hp, maxHp, attack, damageMin, damageMax, defense, speed, posX, posY);
         this.range = range;
-        this.cartridges = cartridges;
+        this.arrows = arrows;
     }
-
-//    Реализовать метод step() лучников.
-//    3.1 Если жизнь равна нулю или стрел нет, завершить обработку.
-//    3.2 Поиск среди противников наиболее приближённого.
-//    3.3 Нанести среднее повреждение найденному противнику.
-//    3.4 Найти среди своих крестьянина.
-//    3.5 Если найден завершить метод иначе уменьшить запас стрел на одну.
     @Override
-    public void step(ArrayList<Human> team1, ArrayList<Human> team2) {
-        if (hp > 0) {
-            if (cartridges > 0) {
-                int index = super.findNearest(team2);
-                makeDamage(team2.get(index));
-                setCartridges(getCartridges()-1);
-            }
-            if (findFarmer(team1)) {
-                cartridges++;
-            }
+    public void step(ArrayList<Human> team1, ArrayList<Human> team2, ArrayList<Barrier> barriers) {
+        if (state.equals("Die") || arrows == 0) return;
+        int index = findNearest(team2);
+        makeDamage(team2.get(index));
+        if (findFarmer(team1)) {
+            return;
         }
+        arrows--;
+    }
+    @Override
+    public StringBuilder getInfo() {
+        StringBuilder builder = new StringBuilder(getProfession());
+        return builder.append(": \t").append(name)
+                .append("\t| ATK:\t").append(attack)
+                .append("\t| HP:\t").append(hp)
+                .append(" \t| Arrows:").append(arrows)
+                .append("\t|")
+                .append("\t|");
+    }
+    @Override
+    public String toString() {
+        return name + "\t" +
+                getEmoji() +
+                "\t| \uD83E\uDE78:" + Math.round(hp) +
+                "\t\uD83D\uDEE1:" + defense +
+                " \t\uD83C\uDFF9:" + attack +
+                " \t\uD83D\uDCA5:" + Math.round(Math.abs((damageMin+damageMax)/2)) +
+                " \t\uD83E\uDEA0:" + arrows;
     }
     protected boolean findFarmer(ArrayList<Human> team) {
-        ArrayList <Human> arrayFarmer = new ArrayList<>();
-        for (int i = 0; i < team.size(); i++) {
-            if (team.get(i).getClass() == Farmer.class) {
-                if(((Farmer) team.get(i)).getCartridgesFarmer() > 0) {
-                    arrayFarmer.add(team.get(i));
-                }
+        ArrayList<Human> arrayFarmer = new ArrayList<>();
+        for (Human human : team) {
+            if (human.getProfession().equals("Фермер") && human.state.equals("Stand")) {
+                arrayFarmer.add(human);
             }
         }
         switch (arrayFarmer.size()) {
-            case (0):
+            case (0) -> {
                 return false;
-            case (1):
-                ((Farmer) arrayFarmer.get(0)).setCartridgesFarmer(0);
+            }
+            case (1) -> {
+                arrayFarmer.get(0).state = "Empty";
                 return true;
-            default:
-                ((Farmer) arrayFarmer.get(findNearest(arrayFarmer))).setCartridgesFarmer(0);
+            }
+            default -> {
+                arrayFarmer.get(findNearest(arrayFarmer)).state = "Empty";
                 return true;
+            }
         }
-    }
-    @Override
-    public void makeDamage (Human unit) {
-        int damage = unit.getDefense() - attack;
-        float hp;
-        if(damage < 0) {
-            hp = unit.getHp() - damageMax;
-            unit.setHp(hp < 0 ? 0: hp);
-        } else if (damage > 0) {
-            hp = unit.getHp() - damageMin;
-            unit.setHp(hp < 0 ? 0: hp);
-        } else {
-            hp = unit.getHp() - ((damageMax+damageMin)/2);
-            unit.setHp(hp < 0 ? 0: hp);
-        }
-    }
-    protected int getCartridges() {
-        return this.cartridges;
-    }
-    protected void setCartridges(int cartridges) {
-        this.cartridges = cartridges;
     }
 }
