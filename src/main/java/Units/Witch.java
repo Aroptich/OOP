49d7ca615cd0c2445976bd42b6dc30848ac75097
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-03-07 13:04:01 +0700

    Update Witch.java

--- src/main/java/Units/Witch.java
+++ src/main/java/Units/Witch.java
@@ -2,26 +2,17 @@ package Units;
 
 public class Witch extends Mag{
 
-    public Witch(String name, float hp, int maxHp, int attack, int damageMin, int damageMax,
-                 int defense, int speed, int mana, int maxMana, int posX, int posY ) {
-        super(name, hp, maxHp, attack, damageMin, damageMax, defense, speed, mana, maxMana);
+    public Witch(String name, Vector2D coords) {
+        super(name, 50.f, 50, 5, -6, -6, 3,
+                7, 60,60, coords.posX, coords.posY);
     }
-    public Witch(String name) {
-        super(name, 130.f, 130, 15, 15, 5, 2,
-                6, 6 ,5);
-    }
-
     @Override
-    public int getAttack() {
-        return attack;
+    public String getProfession() {
+        return "Колдун";
     }
-
     @Override
-    public StringBuilder getInfo() {
-        StringBuilder builder = new StringBuilder();
-        return builder.append("Колдун: \t").append(Witch.super.name)
-                .append("\t| ATK:\t").append(Witch.super.attack)
-                .append("\t| HP:\t").append(Witch.super.hp)
-                .append("\t| MP:\t").append(Witch.super.mana)
-                }
+    public String getEmoji() {
+        if(hp == 0) return "\uD83D\uDC80";
+        return "\u200D\uD83E\uDDDB";
+    }
 }
