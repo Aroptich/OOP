7184ad9052259a85b2ae665343ef6f9a8b7efcfd
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-03-07 13:06:07 +0700

    Create Vector2D.java

---
+++ src/main/java/Units/Vector2D.java
@@ -0,0 +1,41 @@
+package OOP.Units;
+
+
+import java.util.ArrayList;
+
+public class Vector2D {
+    protected int posX;
+    protected int posY;
+
+    public Vector2D(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    protected Double getDistance(Vector2D opponent){
+        return Math.sqrt(Math.pow(posX - opponent.posX,2) + Math.pow(posY - opponent.posY,2));
+    }
+    protected boolean getLeft(Vector2D opponent) {
+        return opponent.posX < posX;
+    }
+    protected boolean getDown(Vector2D opponent) {
+        return opponent.posY < posY;
+    }
+
+    protected boolean getEmpty(ArrayList <Human> team,ArrayList<Barrier> barriers, int posX, int posY) {
+        boolean bool = true;
+        for (Human human: team) {
+            if (human.coords.posX == posX & human.coords.posY == posY) {
+                bool = false;
+                break;
+            }
+        }
+        for (Barrier barr: barriers){
+            if (barr.coords.posX == posX & barr.coords.posY == posY) {
+                bool = false;
+                break;
+            }
+        }
+        return bool;
+    }
+}
