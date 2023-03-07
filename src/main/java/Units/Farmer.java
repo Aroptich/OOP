f22ac703a2690de84a51c7e86d195e553a907973
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-03-07 13:01:06 +0700

    Update Farmer.java

--- src/main/java/Units/Farmer.java
+++ src/main/java/Units/Farmer.java
@@ -1,31 +1,33 @@
 package OOP.Units;
 
+import java.util.ArrayList;
+
 public class Farmer extends Human {
-    protected static int cartridges;
-    public Farmer(String name, float hp, int maxHp, int attack, int damageMin, int damageMax, int defense,
-                  int speed, int cartridges, int posX, int posY) {
-        super(name, hp, maxHp, attack, damageMin, damageMax, defense, speed);
-        this.cartridges = cartridges;
-    }
-    public Farmer(String name) {
-        super(name, 150.f, 1, 1, 1, 1, 1,
-                1);
-        this.cartridges = 1;
-    }
-
-    public void armiger(Shooter Shooter){  }
-
-    static int getCartridges() {
-        return cartridges;
-    }
-    void setCartridges(int o) {
-        this.cartridges = cartridges - o;
+    public Farmer(String name, Vector2D coords) {
+        super(name, 50.f, 50, 1, 1, 1, 1,
+                3, coords.posX, coords.posY);
     }
     @Override
     public StringBuilder getInfo() {
-        StringBuilder builder = new StringBuilder();
-        return builder.append("Фермер: \t").append(Farmer.super.name)
-                .append("\t| ATK:\t").append(Farmer.super.attack)
-                .append("\t| HP:\t").append(Farmer.super.hp)
-               }
+        StringBuilder builder = new StringBuilder(getProfession());
+        return builder.append(": \t").append(name)
+                .append("\t| ATK:\t").append(attack)
+                .append("\t| HP:\t").append(hp)
+                .append(" \t|\t\t\t|")
+                .append("\t|");
+    }
+    @Override
+    public String getProfession() {
+        return "Фермер";
+    }
+    @Override
+    public void step(ArrayList<Human> team1, ArrayList<Human> team2, ArrayList<Barrier> barriers) {
+        if (state.equals("Die")) return;
+        if (state.equals("Empty")) state = "Stand";
+    }
+    @Override
+    public String getEmoji() {
+        if(hp == 0) return "\uD83D\uDC80";
+        return "\uD83E\uDDD1\u200D\uD83C\uDF3E";
+    }
 }
