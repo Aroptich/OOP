5f55aa8f2449cd7befbc2713c896a7ff16e21d99
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-03-07 13:00:09 +0700

    Update Bandit.java

--- src/main/java/Units/Bandit.java
+++ src/main/java/Units/Bandit.java
@@ -1,26 +1,22 @@
 package OOP.Units;
 
+import java.util.ArrayList;
+
 public class Bandit extends Warrior {
-    int disguise;
 
-    public Bandit(String name, float hp, int maxHp, int attack, int damageMin, int damageMax, int defense, int speed, int disguise, int posX, int posY) {
-        super(name, hp, maxHp, attack, damageMin, damageMax, defense, speed);
-        this.disguise = disguise;
-    }
-
-    public Bandit(String name) {
-        super(name, 200.f, 200, 30, 30, 20, 5, 5);
-        this.disguise = 50;
-    }
-
-    public void steal(int disguise, Shooter Shooter) {
+    public Bandit(String name, Vector2D coords) {
+        super(name, 70.f, 70, 10, 2, 6, 7,
+                6, coords.posX, coords.posY);
     }
 
     @Override
-    public StringBuilder getInfo() {
-        StringBuilder builder = new StringBuilder();
-        return builder.append("Бандит: \t").append(Bandit.super.name)
-                .append("\t| ATK:\t").append(Bandit.super.attack)
-                .append("\t| HP:\t").append(Bandit.super.hp)
-                 }
+    public String getProfession() {
+        return "Бандит";
+    }
+
+    @Override
+    public String getEmoji() {
+        if(hp == 0) return "\uD83D\uDC80";
+        return "\uD83E\uDD77";
+    }
 }
