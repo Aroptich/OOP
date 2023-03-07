1776c7d0406134e6515983b657ae2f39e072efa9
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-03-07 13:02:24 +0700

    Update Monk.java

--- src/main/java/Units/Monk.java
+++ src/main/java/Units/Monk.java
@@ -2,27 +2,18 @@ package Units;
 
 public class Monk extends Mag{
 
-    int FreezeStep;
-    public Monk(String name, float hp, int maxHp, int attack, int damageMin, int damageMax,
-                int defense, int speed, int mana, int maxMana) {
-        super(name, hp, maxHp, attack, damageMin, damageMax, defense, speed, mana, maxMana);
-        this.FreezeStep = FreezeStep;
-    }
-    public Monk(String name) {
-        super(name, 130.f, 130, 15, 15, 5, 2,
-                60, 6 ,5);
-        this.FreezeStep = 7;
-    }
-    public int FreezeWitch(Witch witch){
-        return FreezeStep;
-    }
+    public Monk(String name, Vector2D coords) {
+        super(name, 50.f, 50, 6, -7, -7, 3,
+                7, 60 ,60, coords.posX, coords.posY);
 
+    }
     @Override
-    public StringBuilder getInfo() {
-        StringBuilder builder = new StringBuilder();
-        return builder.append("Монах:  \t").append(Monk.super.name)
-                .append("\t| ATK:\t").append(Monk.super.attack)
-                .append("\t| HP:\t").append(Monk.super.hp)
-                .append("\t| MP:\t").append(Monk.super.mana)
-                }
+    public String getProfession() {
+        return "Монах";
+    }
+    @Override
+    public String getEmoji() {
+        if(hp == 0) return "\uD83D\uDC80";
+        return "\uD83E\uDDD9\u200D♂️";
+    }
 }
